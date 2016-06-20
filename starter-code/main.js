fc6523bb7098837b4c41b1d9cd0dd251dae48223
Daniel Landau <danieltotherescue@gmail.com>
2016-06-20 06:13:15 -0700

    javascript function and for loop added

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,8 +5,7 @@ var cardTwo = "king";
 var cardThree = "king";
 var cardFour = "queen";
 
-
-  if (cardOne === cardFour) {
+/*  if (cardOne === cardFour) {
     alert('You found a match!');
   } else if (cardTwo === cardThree) {
     alert('You found a match!');
@@ -14,3 +13,23 @@ var cardFour = "queen";
     alert('Sorry, try again.');
   }
 
+*/
+
+// find the board and set it as a variable
+var board = document.getElementsById(‘game-board’);
+
+//function to create the board
+function createBoard () {
+  // for loop through the card array to create card elements for the board
+  for (var i=0; i<cards.length; i++) {
+
+    // creates a div element which will be used as a card
+    var cardElement = document.createElement('div');
+
+    // add a class to the card element which will help link styling
+    cardElement.className = 'card';
+
+    // append the card to the board
+    board.appendChild(cardElement);
+  }
+}
