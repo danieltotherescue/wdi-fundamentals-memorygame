29d325aad556ae4b9acc5b47ca469997e2c05469
Daniel Landau <danieltotherescue@gmail.com>
2016-06-19 19:55:58 -0700

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,6 @@
 console.log("JS file is connected to HTML! Woo!")
+
+var cardOne = "queen";  //mixed order to provide a more challenging game
+var cardTwo = "king";
+var cardThree = "king";
+var cardFour = "queen";
