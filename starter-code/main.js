41375ca1df6da18e2c1675d9e013865dd4ea574c
Daniel Landau <danieltotherescue@gmail.com>
2016-06-19 21:28:40 -0700

    javascript alerts added

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,13 @@ var cardOne = "queen";  //mixed order to provide a more challenging game
 var cardTwo = "king";
 var cardThree = "king";
 var cardFour = "queen";
+
+
+  if (cardOne === cardFour) {
+    alert('You found a match!');
+  } else if (cardTwo === cardThree) {
+    alert('You found a match!');
+  } else {
+    alert('Sorry, try again.');
+  }
+
